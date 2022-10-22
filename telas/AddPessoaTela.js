307b921f3c7acf54ec3a3222bc23b80c6b4cc7e7
Rodrigo Bossini <professorbossini@gmail.com>
2022-10-22 08:02:22 -0300

    comecando com react navigation

---
+++ telas/AddPessoaTela.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import AddPessoaComponent from '../components/AddPessoaComponent'
+
+const AddPessoaTela = () => {
+  return (
+    <View>
+      <AddPessoaComponent />
+    </View>
+  )
+}
+
+export default AddPessoaTela
